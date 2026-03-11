1e75719bfe0da6630c2d06cb05f66d78dddbe816
BlackScull12 <87164998+BlackScull12@users.noreply.github.com>
2026-03-12 00:46:05 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from 
+"https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+
+import { getAuth } from 
+"https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+
+import { getFirestore } from 
+"https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+apiKey: "YOUR_KEY",
+authDomain: "YOUR_DOMAIN",
+projectId: "YOUR_PROJECT",
+storageBucket: "YOUR_BUCKET",
+messagingSenderId: "YOUR_ID",
+appId: "YOUR_APP"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
