1ea25d801749d55223307177d4dac83dc635e771
BlackScull12 <87164998+BlackScull12@users.noreply.github.com>
2026-03-12 11:03:31 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,9 +1,8 @@
-import { auth, db } from "./firebase.js";
+import { auth,db } from "./firebase.js";
 
 import {
 GoogleAuthProvider,
-signInWithPopup,
-onAuthStateChanged
+signInWithPopup
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 import {
@@ -11,17 +10,26 @@ doc,
 setDoc
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-const provider = new GoogleAuthProvider();
+const provider=new GoogleAuthProvider();
 
-document.getElementById("loginBtn").onclick = async () => {
+const loginBtn=document.getElementById("loginBtn");
 
-const result = await signInWithPopup(auth, provider);
-const user = result.user;
+loginBtn.onclick=async()=>{
+
+const result=await signInWithPopup(auth,provider);
+
+const user=result.user;
 
 await setDoc(doc(db,"users",user.uid),{
+
 name:user.displayName,
 email:user.email
+
 });
+
+window.location="chat.html";
+
+};
 
 window.location="chat.html";
 
