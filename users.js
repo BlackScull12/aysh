076b08614f062ed1bd33c7f4934aec3369cdd3eb
BlackScull12 <87164998+BlackScull12@users.noreply.github.com>
2026-03-12 11:36:00 +0530

    Update users.js

--- users.js
+++ users.js
@@ -1,44 +1,55 @@
-import { db,auth } from "./firebase.js";
+import { db, auth } from "./firebase.js";
 
 import {
 collection,
 getDocs
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-const usersList=document.getElementById("usersList");
+import {
+onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
-async function loadUsers(){
+const usersList = document.getElementById("usersList");
 
-const snapshot=await getDocs(collection(db,"users"));
+/* WAIT FOR AUTHENTICATION */
 
-usersList.innerHTML="";
+onAuthStateChanged(auth, (user) => {
 
-snapshot.forEach((docu)=>{
+if(!user){
 
-const user=docu.data();
-
-if(auth.currentUser && docu.id===auth.currentUser.uid) return;
-
-const div=document.createElement("div");
-
-div.innerText=user.name;
-
-div.style.padding="10px";
-div.style.cursor="pointer";
-
-div.onclick=()=>{
-
-localStorage.setItem("chatUser",docu.id);
-localStorage.setItem("chatName",user.name);
-
-location.reload();
-
-};
-
-usersList.appendChild(div);
-
-});
+window.location.href = "index.html";
+return;
 
 }
 
-setTimeout(loadUsers,1000);
+loadUsers(user);
+
+});
+
+/* LOAD USERS */
+
+async function loadUsers(currentUser){
+
+try{
+
+const snapshot = await getDocs(collection(db,"users"));
+
+usersList.innerHTML = "";
+
+snapshot.forEach((docu)=>{
+
+const user = docu.data();
+
+/* DON'T SHOW CURRENT USER */
+
+if(docu.id === currentUser.uid) return;
+
+const div = document.createElement("div");
+
+div.innerText = user.name;
+
+div.style.padding = "12px";
+div.style.cursor = "pointer";
+div.style.borderBottom = "1px solid #333";
+
+/* OPEN CHA*
