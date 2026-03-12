71072b0e10429c2d278a0bef5f1e6fc10ed8a0f0
BlackScull12 <87164998+BlackScull12@users.noreply.github.com>
2026-03-12 10:30:54 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,7 +7,14 @@ import { getAuth } from
 import { getFirestore } from 
 "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-
+const firebaseConfig = {
+  apiKey: "AIzaSyAxt94UyMn8AP8PFaSHPJ29JnZQ2KI3kZw",
+  authDomain: "chatgithub-e838d.firebaseapp.com",
+  projectId: "chatgithub-e838d",
+  storageBucket: "chatgithub-e838d.firebasestorage.app",
+  messagingSenderId: "755589384017",
+  appId: "1:755589384017:web:6af4c6d223d646cf36f570"
+};
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
